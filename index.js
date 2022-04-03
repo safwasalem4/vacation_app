df3616d662341b02c8b0bb538f830f4571184af7
Safwa Salem <safwa.salem@gmail.com>
2022-04-04 01:34:44 +0200

    add app screens navigation

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { name as appName } from './app.json';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import configureStore from './src/redux/store/configureStore';
+
+const store = configureStore();
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
